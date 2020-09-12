875b34ed1c32c9eed94aaf3a6b02af8377ea6e2c
Chrisebell24 <31806186+Chrisebell24@users.noreply.github.com>
2020-09-12 11:49:00 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,13 +7,13 @@ chmod 644 /etc/resolv.conf
 exit
 
 # General
-sudo apt-get update
+yes | sudo apt-get update
 
 # Install SSH & Firewall Settings
-sudo apt-get install openssh-server && sudo systemctl enable ssh && sudo systemctl start ssh && sudo ufw allow 22 && sudo ufw allow 4000 && sudo ufw allow 8888
+yes | sudo apt-get install openssh-server && sudo systemctl enable ssh && sudo systemctl start ssh && sudo ufw allow 22 && sudo ufw allow 4000 && sudo ufw allow 8888
 
 # Install No Machine
-sudo apt-get install alien && wget https://download.nomachine.com/download/6.11/Raspberry/nomachine_6.11.2_1_armhf.deb && sudo dpkg -i nomachine_6.11.2_1_armhf.deb
+yes | sudo apt-get install alien && yes | wget https://download.nomachine.com/download/6.11/Raspberry/nomachine_6.11.2_1_armhf.deb && yes | sudo dpkg -i nomachine_6.11.2_1_armhf.deb
 
 # Install Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg2 && curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && sudo apt install docker-ce && sudo systemctl status docker
+yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg2 && yes | curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - && yes | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && yes | sudo apt install docker-ce && sudo systemctl status docker
